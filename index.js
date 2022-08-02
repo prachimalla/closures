b0e23f47b0edcdc50bdf74a2321086ff1ae0dfdb
prachimalla <prachimalla7@gmail.com>
2022-08-02 16:24:53 +0530

    1 file modified

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-
-function sum(a){
-  return function(b){
-    if(b) {
+function sum(a) {
+  return function (b) {
+    if (b) {
       return sum(a + b);
-    } else  {
-      return a
-    };
-
-  }
+    } else {
+      return a;
+    }
+  };
 }
-console.log(sum(1)())
+console.log(sum(1)());
+
+//converts f(a,b,c) into a curried function f(a)(b)(c)
