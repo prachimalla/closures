7b59c97c215abecfba13dcf70154b2ca8932da7f
prachimalla <prachimalla7@gmail.com>
2022-08-02 09:30:55 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+
+function sum(a){
+  return function(b){
+    if(b) {
+      return sum(a + b);
+    } else  {
+      return a
+    };
+
+  }
+}
+console.log(sum(1)())
